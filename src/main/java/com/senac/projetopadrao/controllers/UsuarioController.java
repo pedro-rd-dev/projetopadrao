70c1127f432e04508006269c48a5d0683c3c3dcd
pedro-rd-dev <pedro.rd.dev@gmail.com>
2021-05-17 21:57:13 -0300

    iNSERINDO USUARIOS

--- src/main/java/com/senac/projetopadrao/controllers/UsuarioController.java
+++ src/main/java/com/senac/projetopadrao/controllers/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.senac.projetopadrao.controllers;
 
+import com.senac.projetopadrao.models.Usuario;
 import com.senac.projetopadrao.repositorys.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 
 @Controller
@@ -15,6 +18,26 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @GetMapping("/")
+    public ModelAndView listarUsuarios(){
+        ModelAndView mv = new ModelAndView("usuario");
+
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+
+        usuarios = (ArrayList<Usuario>) usuarioRepository.findAll();
+
+        mv.addObject("usuarios",usuarios);
+
+        return mv;
+    }
+
+
+/*
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    */
+
+/*
     @GetMapping("/")
     public ModelAndView listarUsuarios(){
         ModelAndView mv = new ModelAndView("usuarios");
@@ -27,4 +50,6 @@ public class UsuarioController {
 
         return "usuarios_add";
     }
+    */
+
 }
